af53a29e624fe6e8d2817b7d92f36e6be40fb055
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-06-19 21:37:00 +0300

    Moved many file-handling functions to z-file.

--- src/externs.h
+++ src/externs.h
@@ -834,24 +834,6 @@ extern void store_maint(int which);
 extern void store_init(int which);
 
 /* util.c */
-extern errr path_parse(char *buf, int max, const char * file);
-extern errr path_temp(char *buf, int max);
-extern errr path_build(char *buf, int max, const char * path, const char * file);
-extern FILE *my_fopen(const char * file, const char * mode);
-extern FILE *my_fopen_temp(char *buf, size_t max);
-extern errr my_fclose(FILE *fff);
-extern errr my_fgets(FILE *fff, char *buf, size_t n);
-extern errr my_fputs(FILE *fff, const char * buf, size_t n);
-extern errr fd_kill(const char * file);
-extern errr fd_move(const char * file, const char * what);
-extern errr fd_copy(const char * file, const char * what);
-extern int fd_make(const char * file, int mode);
-extern int fd_open(const char * file, int flags);
-extern errr fd_lock(int fd, int what);
-extern errr fd_seek(int fd, long n);
-extern errr fd_read(int fd, char *buf, size_t n);
-extern errr fd_write(int fd, const char * buf, size_t n);
-extern errr fd_close(int fd);
 extern errr check_modification_date(int fd, const char * template_file);
 extern void text_to_ascii(char *buf, const char * str);
 extern void ascii_to_text(char *buf, const char * str);
--- src/z-file.h
+++ src/z-file.h
@@ -13,6 +13,26 @@
 extern int player_uid;
 extern int player_egid;
 
+extern errr path_parse(char *buf, int max, const char * file);
+extern errr path_temp(char *buf, int max);
+extern errr path_build(char *buf, int max, const char * path, const char * file);
+extern FILE *my_fopen(const char * file, const char * mode);
+extern FILE *my_fopen_temp(char *buf, size_t max);
+extern errr my_fclose(FILE *fff);
+extern errr my_fgets(FILE *fff, char *buf, size_t n);
+extern errr my_fputs(FILE *fff, const char * buf, size_t n);
+extern errr fd_kill(const char * file);
+extern errr fd_move(const char * file, const char * what);
+extern errr fd_copy(const char * file, const char * what);
+extern int fd_make(const char * file, int mode);
+extern int fd_open(const char * file, int flags);
+extern errr fd_lock(int fd, int what);
+extern errr fd_seek(int fd, long n);
+extern errr fd_read(int fd, char *buf, size_t n);
+extern errr fd_write(int fd, const char * buf, size_t n);
+extern errr fd_close(int fd);
+
+
 void safe_setuid_drop(void);
 void safe_setuid_grab(void);
 

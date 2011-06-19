21379b86f0ebf7ba53d262fb1b91740acbf27f9e
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-06-19 19:22:45 +0300

    Updated and cleaned z-util.

--- src/z-util.h
+++ src/z-util.h
@@ -14,13 +14,6 @@
 #include "h-basic.h"
 
 
-/*
- * Extremely basic stuff, like global temp and constant variables.
- * Also, some very useful low level functions, such as "streq()".
- * All variables and functions in this file are "addressable".
- */
-
-
 /**** Available variables ****/
 
 /**
@@ -44,35 +37,53 @@ extern int my_stricmp(const char *s1, const char *s2);
 /**
  * Case insensitive comparison between two strings, up to n characters long.
  */
-extern int my_strnicmp(const char * a, const char * b, int n);
+extern int my_strnicmp(const char *a, const char *b, int n);
 
 /**
  * Case-insensitive strstr
  */
 extern char *my_stristr(const char *string, const char *pattern);
 
-/* Copy a string */
+/**
+ * Copy up to 'bufsize'-1 characters from 'src' to 'buf' and NULL-terminate
+ * the result.  The 'buf' and 'src' strings may not overlap.
+ *
+ * Returns: strlen(src).  This makes checking for truncation
+ * easy.  Example:
+ *   if (my_strcpy(buf, src, sizeof(buf)) >= sizeof(buf)) ...;
+ *
+ * This function should be equivalent to the strlcpy() function in BSD.
+ */
 extern size_t my_strcpy(char *buf, const char *src, size_t bufsize);
 
-/* Concatenate two strings */
+/**
+ * Try to append a string to an existing NULL-terminated string, never writing
+ * more characters into the buffer than indicated by 'bufsize', and
+ * NULL-terminating the buffer.  The 'buf' and 'src' strings may not overlap.
+ *
+ * my_strcat() returns strlen(buf) + strlen(src).  This makes checking for
+ * truncation easy.  Example:
+ *   if (my_strcat(buf, src, sizeof(buf)) >= sizeof(buf)) ...;
+ *
+ * This function should be equivalent to the strlcat() function in BSD.
+ */
 extern size_t my_strcat(char *buf, const char *src, size_t bufsize);
 
 /* Test equality, prefix, suffix */
-extern bool streq(const char * s, const char * t);
-extern bool prefix(const char * s, const char * t);
-extern bool suffix(const char * s, const char * t);
+extern bool streq(const char *s, const char *t);
+extern bool prefix(const char *s, const char *t);
+extern bool prefix_i(const char *s, const char *t);
+extern bool suffix(const char *s, const char *t);
 
 
 /* Print an error message */
-extern void plog(const char * str);
+extern void plog(const char *str);
 
 /* Exit, with optional message */
-extern void quit(const char * str);
+extern void quit(const char *str);
 
 
 /* Sorting functions */
-/* TODO: make ang_sort() take comp and swap hooks rather than use globals */
-/* void ang_sort(void *u, void *v, int n); */
 extern void sort(void *array, size_t nmemb, size_t smemb,
 		 int (*comp)(const void *a, const void *b));
 
